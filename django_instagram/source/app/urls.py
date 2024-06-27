a08b0509f95ef2405b7001f9abebe92f329b2903
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2024-06-27 21:31:30 +0300

    adding urls.py

---
+++ django_instagram/source/app/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path
+
+from app.views.user import CreateUser, DestroyUser, LoginUserView, RetrieveUser, UpdateUser, RetrieveCurrentUserProfile
+from app.views.post import CommentPost, CreatePost, DestroyPost, FollowUser, RetrievePost  
+from app.views.post import RetrieveUserPosts, UpdatePost, LikePost, GetAllPosts , ListNotifications , HandleNotification
+
+urlpatterns = [
+    path('user/create/', CreateUser.as_view()),
+    path('user/login/', LoginUserView.as_view()),
+    path('user/profile/', RetrieveCurrentUserProfile.as_view()),
+    path('user/<int:pk>/', RetrieveUser.as_view()),
+    path('user/update/', UpdateUser.as_view()),
+    path('user/delete/<int:pk>', DestroyUser.as_view()),
+
+    path('posts/', RetrieveUserPosts.as_view()),
+    path('posts/all/', GetAllPosts.as_view()),
+    path('post/create/', CreatePost.as_view()),
+    path('post/<int:pk>/', RetrievePost.as_view()),
+    path('post/update/<int:pk>/', UpdatePost.as_view()),
+    path('post/delete/<int:pk>/', DestroyPost.as_view()),
+
+    path('post/like/<int:pk>/', LikePost.as_view()),
+    path('notifications/', ListNotifications.as_view(), name='list-notifications'),
+    path('notifications/<int:pk>/', HandleNotification.as_view(), name='handle-notification'),
+
+    path('post/comment/<int:pk>/', CommentPost.as_view()),
+    path('post/comments/<int:pk>/', CommentPost.as_view()),
+
+
+    path('user/follow/<int:pk>/', FollowUser.as_view()),
+]
