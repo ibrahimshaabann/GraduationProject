c0825c9c2640e50b0e1c16291be728856701f7d1
Ibrahim Shaaban <ibrahemhema2017@gmail.com>
2024-07-02 01:18:55 +0300

    fixing notification_serializer variable error2

--- django_instagram/source/app/signals.py
+++ django_instagram/source/app/signals.py
@@ -15,41 +15,37 @@ def post_save_post(sender, instance, created, **kwargs):
             image_hash = dhash.format_hex(row, col)
             instance.image_hash = image_hash
             instance.save()
+
             # Check for similar images
             similar_posts = bktree.query(image_hash, tolerance=3)
-            print("#"*30)
+            print("#" * 30)
             print(f"\n\n\similar posts value:{similar_posts}\n\n\n")
-            print("#"*30)
+            print("#" * 30)
 
-          
+            notification_data = None
             if similar_posts:
-                print("*"*30)
+                print("*" * 30)
                 print(f"\n\n\nsimilarrrrrrrrrrrrrrr!!!!\n\n\n")
-                print("*"*30)
+                print("*" * 30)
                 original_post_id = similar_posts[0][0]
                 original_post = Post.objects.get(id=original_post_id)
                 notification_data = {
-                    'user': original_post.user.pk ,
+                    'user': original_post.user.pk,
                     'post': instance.id,
-                    'action_taken': None  
+                    'action_taken': None
                 }
 
-            notification_serializer = NotificationSerializer(data=notification_data)
-
-            if notification_serializer.is_valid():
+            if notification_data:
+                notification_serializer = NotificationSerializer(data=notification_data)
+                if notification_serializer.is_valid():
                     try:
                         notification_instance = notification_serializer.save()
-                        
                         print(f"Notification saved successfully: {notification_instance}")
                     except Exception as e:
-                       
                         print(f"Error saving notification: {e}")
-            else:
+                else:
                     print(f"Validation errors: {notification_serializer.errors}")
 
-            
-            instance.image_hash = image_hash
-            instance.save()
             bktree.insert(instance.image_hash, instance.id)
 
         except Exception as e:
